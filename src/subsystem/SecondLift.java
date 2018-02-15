ff8a354ded4b73adeacde053a477e6251b35d164
2472Centurions <2472centurions@gmail.com>
2018-02-15 17:23:05 -0600

    Revert "Revert "added subsystems""
    This reverts commit f7769a29736445d5813ccf5b5209cf0fe8d2d93a.

---
+++ src/subsystem/SecondLift.java
@@ -0,0 +1,14 @@
+package subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import constants.Const;
+public class SecondLift {
+	TalonSRX liftMotor=new TalonSRX(Const.lift);
+
+public void lift(double speed) 
+{
+liftMotor.set(ControlMode.PercentOutput,speed);	
+}
+	}
